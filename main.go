6b3045de92d7a0b8da4b569a238b32fa2e258aeb
Daniel Leong <me@dhleong.net>
2018-04-29 11:11:34 -0400

    Add some CLI help + embed a version number

--- main.go
+++ main.go
@@ -1,13 +1,53 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/docopt/docopt-go"
 	"github.com/rivo/tview"
 
 	beholder "github.com/dhleong/beholder/src"
 	"github.com/dhleong/beholder/src/ui"
 )
 
+const beholderVersion = "0.1.0"
+
+type options struct {
+	// right now we only have the --version and --help options
+}
+
+func parseOptions() *options {
+	usage := `beholder: A CLI tool for D&D players
+
+Usage: beholder
+
+That's it! Just run the command and start typing to search for whatever
+you're curious about. Use <ctrl-k> and <ctrl-k> to scroll through
+results. If the information doesn't fit entirely in the info pane, you
+can focus on it by pressing <enter>.
+
+When focused on the info pane, you can use the arrow keys, page up/down,
+or j/k to scroll; press <esc> or <backspace> to return focus to the
+search bar.
+
+Options:
+  -h, --help  Show this screen.
+  --version   Show version.`
+	args, _ := docopt.ParseArgs(
+		usage,
+		os.Args[1:],
+		fmt.Sprintf("beholder version %s", beholderVersion),
+	)
+
+	opts := &options{}
+	args.Bind(opts)
+	return opts
+}
+
 func main() {
+
+	parseOptions()
 
 	dataSource, err := beholder.NewDataSource()
 	if err != nil {
