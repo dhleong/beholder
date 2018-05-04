f682dce017d63b8ae6e0a901da7baf04f0accc8b
Daniel Leong <me@dhleong.net>
2018-05-04 18:09:23 -0400

    Improve the "home" UI slightly
    Finally vertically centered, plus some little accents. At some
    point it'd be nice to have proper color schemes....

--- src/ui/home-ui.go
+++ src/ui/home-ui.go
@@ -12,28 +12,49 @@ type HomeUI struct {
 	UI tview.Primitive
 }
 
+type verticalCenterLayout struct {
+	*tview.Box
+	child       *tview.TextView
+	childHeight int
+}
+
+func (l *verticalCenterLayout) Draw(screen tcell.Screen) {
+	x, y, w, h := l.GetRect()
+
+	childTop := (h / 2) - (l.childHeight / 2)
+
+	l.child.SetRect(x, y+childTop, w, l.childHeight)
+	l.child.Draw(screen)
+}
+
 // NewHomeUI .
 func NewHomeUI() *HomeUI {
 
 	text := tview.NewTextView().
+		SetDynamicColors(true).
 		SetTextAlign(tview.AlignCenter).
 		SetTextColor(tcell.ColorDefault).
 		SetText(strings.Trim(`
-Welcome to Beholder!
+[::b]Welcome to Beholder![::-]
 
 Just start typing to search for an item,
 monster, spell, feat, rule, condition,
 or class or race feature.
 
-Press F1 or ? for detailed help anywhere.
+Press [::b]F1[::-] or [::b]?[::-] for detailed help anywhere.
 `, " \n"))
 
-	flex := tview.NewFlex().
-		SetDirection(tview.FlexRow)
-	flex.SetBorderPadding(2, 1, 2, 2)
-	flex.AddItem(text, 0, 1, false)
+	// flex := tview.NewFlex().
+	// 	SetDirection(tview.FlexRow)
+	// flex.SetBorderPadding(2, 1, 2, 2)
+	// flex.AddItem(text, 0, 1, false)
 
 	return &HomeUI{
-		UI: flex,
+		// UI: flex,
+		UI: &verticalCenterLayout{
+			Box:         tview.NewBox(),
+			child:       text,
+			childHeight: 7,
+		},
 	}
 }
