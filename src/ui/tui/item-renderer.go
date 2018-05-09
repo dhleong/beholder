3d113f511bbc896082040904f7203792160fdcac
Daniel Leong <me@dhleong.net>
2018-05-09 18:35:34 -0400

    Render magic-ness and value of items
    Magic items without a value show the recommended range of values
    for a magic item of that rarity

--- src/ui/tui/item-renderer.go
+++ src/ui/tui/item-renderer.go
@@ -7,6 +7,8 @@ import (
 	beholder "github.com/dhleong/beholder/src"
 )
 
+var valueSeparator = "  "
+
 // ItemRenderer can render an Item
 var ItemRenderer = &EntityRenderer{
 	replacements: func(e beholder.Entity) []string {
@@ -16,7 +18,46 @@ var ItemRenderer = &EntityRenderer{
 			"{rarity}", i.Rarity,
 		}
 
+		value := i.Value
+		if value == "" && i.Magic > 0 && i.Rarity != "" {
+			// suggested magic item price ranges
+			switch i.Rarity {
+			case "Common":
+				value = "50—100 gp"
+			case "Uncommon":
+				value = "101-500 gp"
+			case "Rare":
+				value = "501-5000 gp"
+			case "Very Rare":
+				value = "5001-50000 gp"
+			case "Legendary":
+				value = "50000+ gp"
+			}
+		}
+
+		actualSeparator := ""
+		if value != "" {
+			rs = append(rs, "{value}", fmt.Sprintf(
+				"[::d]%s[-:-:-]", value,
+			))
+		} else {
+			rs = append(rs, "{value}", "")
+		}
+
+		if i.Magic > 0 {
+			rs = append(rs, "{magic}", " (magic)")
+			if value != "" {
+				actualSeparator = valueSeparator
+			}
+		} else {
+			rs = append(rs, "{magic}", "")
+		}
+
 		if i.Rarity != "" {
+			if value != "" {
+				actualSeparator = valueSeparator
+			}
+
 			rs = append(rs, "{text}", strings.Replace(
 				strings.Join(i.GetText(), "\n"),
 				fmt.Sprintf("Rarity: %s\n", i.Rarity),
@@ -25,12 +66,14 @@ var ItemRenderer = &EntityRenderer{
 			))
 		}
 
+		rs = append(rs, "{value-sep}", actualSeparator)
+
 		return rs
 	},
 
 	template: `
 [::bu]{name}[-:-:-]
-[::d]{rarity}[-:-:-]
+[::d]{rarity}{magic}[-:-:-]{value-sep}{value}
 
 {text}
 `,
